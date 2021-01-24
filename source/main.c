3cf884327e784a82f6fb4693b7acd8cf0adf3bc5
Al-Azif <alazif@miskatonic.io>
2021-01-23 16:17:55 -0800

    Add debug socket

--- source/main.c
+++ source/main.c
@@ -1,3 +1,7 @@
+//#define DEBUG_SOCKET
+#define DEBUG_IP "192.168.2.2"
+#define DEBUG_PORT 9023
+
 #include "ps4.h"
 
 int _main(struct thread *td) {
@@ -5,6 +9,11 @@ int _main(struct thread *td) {
 
   initKernel();
   initLibc();
+
+#ifdef DEBUG_SOCKET
+  initNetwork();
+  DEBUG_SOCK = SckConnect(DEBUG_IP, DEBUG_PORT);
+#endif
 
   jailbreak();
 
@@ -56,6 +65,12 @@ int _main(struct thread *td) {
   }
 
   printf_notification("Restore complete!\nRebooting console...");
+
+#ifdef DEBUG_SOCKET
+  printf_socket("\nClosing socket...\n\n");
+  SckClose(DEBUG_SOCK);
+#endif
+
   sceKernelSleep(10);
   reboot();
 
